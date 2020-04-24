47d3c33197fe69345fce47a233e729a72c325432
Fran Martin <38869988+manudiv16@users.noreply.github.com>
2020-04-24 19:40:16 +0200

    👻

--- src/utils/csv_utils.py
+++ src/utils/csv_utils.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 from csv import DictReader
-from typing import List, Dict, Union
+from typing import List, Dict
 
 
 def read_csv(file_path: str) -> List:
@@ -13,7 +13,7 @@ def read_csv(file_path: str) -> List:
 
 # TODO: modify sorted, only if it is necessary.
 
-def get_average_row(values : tuple) -> int:
+def get_average_row(values: tuple) -> int:
     sorted_values = sorted(values, key=lambda x: x.length)
     average_value = sorted_values[len(values) // 2]
     return average_value.row
@@ -22,13 +22,14 @@ def get_average_row(values : tuple) -> int:
 def filter_country_average_length(data: List[Dict]) -> List[Dict]:
     """
     calculate the average length of regions
-    :param csv_data: A csv with the regions
+    :param data: A csv with the regions
     :return: list csv with the average length on region
     """
     country_dict = dict()
-    named_tuple = namedtuple("tuple", "row length")
+    named_sample = namedtuple("data_info", "row length")
     for row, sample in enumerate(data):
         country, length = sample.get('Geo_Location', 'Unknown'), sample['Length']
-        country_dict.setdefault(country, []).append(named_tuple(row, length))
-    average_rows = [get_average_row(country_dict[country]) for country in country_dict]    
-    return [data[row] for row in average_rows]
+        country_dict.setdefault(country, []).append(named_sample(row, length))
+    average_rows = [get_average_row(country_dict[country]) for country in country_dict]
+    average_rows_ = [data[row] for row in average_rows]
+    return average_rows_
