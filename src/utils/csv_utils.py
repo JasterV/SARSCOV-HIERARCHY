b370a0fd305b738a716ecc5b0fd6acdaaebc4e34
JasterV <victorcoder2@gmail.com>
2020-04-24 16:26:05 +0200

    2 funcions afegides al csv

--- src/utils/csv_utils.py
+++ src/utils/csv_utils.py
@@ -35,3 +35,23 @@ def country_dict(csv_data: List[Dict]) -> Dict:
     target_samples = {c: countries_ordered[c][len(countries_ordered[c]) // 2]
                       for c in countries_ordered}
     return target_samples
+
+
+def map_average(country_dict: dict) -> Dict:
+    new_dict = dict()
+    for country, value in country_dict.items():
+        sorted_samples = sorted(value, key=lambda x: x[1])
+        average = sorted_samples[len(sorted_samples) // 2]
+        target_id = average[0]
+        new_dict[country] = target_id
+    return new_dict
+
+
+def filter_country_average_length(data: List[Dict]) -> List[Dict]:
+    country_dict = dict()
+    for sample in data:
+        rna_id, country, length = sample['Accession'], sample['Geo_Location'], sample['Length']
+        country_dict.setdefault(country, []).append((rna_id, length))
+    filtered_dict = map_average(country_dict)
+    return list(filter(lambda sample: sample['Accession'] in filtered_dict.values(), data))
+
