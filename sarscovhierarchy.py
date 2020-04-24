f3bbfdfad6e1aa78da0d2a05b80f7f4611208383
Fran Martin <38869988+manudiv16@users.noreply.github.com>
2020-04-24 02:30:34 +0200

    implement average length regions and minimum refactor

--- sarscovhierarchy.py
+++ sarscovhierarchy.py
@@ -1,7 +1,7 @@
-from sys import argv, exit
 from os.path import join
-from data_reader import read_csv, read_fasta
+from sys import argv, exit
 
+from data_tratamiento.data_reader import read_csv, read_fasta, country_dict, recur_country_dict
 
 if __name__ == '__main__':
     if len(argv) != 2:
@@ -14,3 +14,6 @@ if __name__ == '__main__':
 
     fasta_data = read_fasta(fasta_path)
     csv_data = read_csv(csv_path)
+    medium = country_dict(csv_data)
+    medium2 = recur_country_dict(csv_data)
+    print(medium2)
