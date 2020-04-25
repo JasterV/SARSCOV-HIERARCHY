f38ca2c2f96042e8d1f25003cfc22d16e5d5a8f4
Fran Martin <38869988+manudiv16@users.noreply.github.com>
2020-04-25 16:48:57 +0200

    create Csv class

--- src/sarscovhierarchy.py
+++ src/sarscovhierarchy.py
@@ -1,6 +1,6 @@
 from os.path import join
 from sys import argv, exit
-from utils.csv_utils import read_csv, filter_country_average_length
+from utils.csv_utils import CSV
 from utils.fasta_utils import Fasta
 
 if __name__ == '__main__':
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     fasta_path = join(data_dir, "sequences.fasta")
 
     fasta = Fasta(fasta_path)
-    csv_data = read_csv(csv_path)
+    csv_data = CSV(csv_path)
 
     filtered_data = filter_country_average_length(csv_data)
 
--- src/utils/csv_utils.py
+++ src/utils/csv_utils.py
@@ -1,35 +1,43 @@
 from collections import namedtuple
 from csv import DictReader
-from typing import List, Dict, Union
+from typing import List, Union
 
 
-def read_csv(file_path: str) -> List:
-    """Reads a csv file and returns a list of Ordered Maps
-    """
-    with open(file_path, 'r') as csv_file:
-        reader = DictReader(csv_file, delimiter=',')
-        return list(reader)
+class CSV:
+    def __init__(self, path_file=None, table=None):
+        if path_file is not None:
+            self.__csv_list = self.__read_csv(path_file)
+        elif table is not None:
+            self.__csv_list = table
+        else:
+            raise TypeError("Invalid arguments")
 
+    @staticmethod
+    def __read_csv(file_path: str) -> List:
+        """Reads a csv file and returns a list of Ordered Maps
+        """
+        with open(file_path, 'r') as csv_file:
+            reader = DictReader(csv_file, delimiter=',')
+            return list(reader)
 
-# TODO: modify sorted, only if it is necessary.
+    # TODO: modify sorted, only if it is necessary.
 
-def get_average_row(csv_data: List[Dict], values: tuple) -> Union[dict, List[dict]]:
-    sorted_values = sorted(values, key=lambda x: x.length)
-    average_value = sorted_values[len(values) // 2]
-    return csv_data[average_value.row]
+    def __get_average_row(self, values: tuple) -> Union[dict, List[dict]]:
+        sorted_values = sorted(values, key=lambda x: x.length)
+        average_value = sorted_values[len(values) // 2]
+        return self.__csv_list[average_value.row]
 
-
-def filter_country_average_length(data: List[Dict]) -> List[Union[dict, List[dict]]]:
-    """
-    calculate the average length of regions
-    :param data: A csv with the regions
-    :return: list csv with the average length on region
-    """
-    country_dict = dict()
-    named_sample = namedtuple("data_info", "row length")
-    for row, sample in enumerate(data):
-        country, length = sample.get(
-            'Geo_Location', 'Unknown'), sample['Length']
-        country_dict.setdefault(country, []).append(named_sample(row, length))
-    return [get_average_row(data, country_dict[country])
-            for country in country_dict]
+    def filter(self):
+        """
+        calculate the average length of regions
+        :return: csv instance with the average length on region
+        """
+        country_dict = dict()
+        named_sample = namedtuple("data_info", "row length")
+        for row, sample in enumerate(self.__csv_list):
+            country, length = sample.get(
+                'Geo_Location', 'Unknown'), sample['Length']
+            country_dict.setdefault(country, []).append(named_sample(row, length))
+        filtered_data = [self.__get_average_row(country_dict[country])
+                         for country in country_dict]
+        return CSV(table=filtered_data)
