618973e770784f6caebd67ee484aab68ea404085
manudiv16 <manudiv16@gmail.com>
2020-04-27 12:09:26 +0200

    multiprocessing some errors

--- src/utils/fasta_map.py
+++ src/utils/fasta_map.py
@@ -1,3 +1,5 @@
+import concurrent.futures
+from collections import namedtuple
 from typing import Tuple, Dict, List, Union, Any
 
 from utils.csv_table import CsvTable
@@ -53,23 +55,33 @@ class FastaMap:
         genome_id = header.split('|')[0].strip()
         return genome_id, genome
 
-    @staticmethod
-    def group_samples(csv_table: CsvTable) -> List[Union[set, Any]]:
+    def group_samples(self, csv_table: CsvTable) -> List[Union[set, Any]]:
         """Estructura del que pot ser la funció de creació de sets
         """
-        # groups = list()
+
+        to_compare = []
         compares = dict()
         for i, sample_first in enumerate(csv_table):
             for sample_two in csv_table[1 + i:]:
                 id1, id2 = sample_first['Accession'], sample_two['Accession']
-                result = FastaMap.compare_samples(id1, id2)
-                if result > 0.9:
-                    compares.setdefault(id1, set())
-                    compares[id1].add(id2)
-
+                to_compare.append((id1, id2))
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            results = executor.map(self.compare_multi, to_compare)
+        print(results)
+        for x in results:
+            compares.setdefault(x.id1, set())
+            compares[x.id1].add(x.id2)
         list_relations = FastaMap.generate_relations(compares)
 
         return list_relations
+
+    def compare_multi(self, ids):
+        named_compare = namedtuple("comparation", "id1 id2")
+        result = self.compare_samples(ids[0], ids[1])
+        if result > 0.9:
+            r = named_compare(ids[0], ids[1])
+        print(r)
+        return r
 
     @staticmethod
     def generate_relations(compares):
