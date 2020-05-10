108035cf884c76f2e6daf7e5b2b7777cad8f1add
JasterV <victorcoder2@gmail.com>
2020-05-10 20:17:33 +0200

    sequence alignment implemented

--- src/sarscovhierarchy.py
+++ src/sarscovhierarchy.py
@@ -4,49 +4,6 @@ from typing import Any, List, Union
 from utils.csv_table import CsvTable
 from utils.fasta_map import FastaMap
 
-#
-# def group_samples(fasta: FastaMap, csv_table: CsvTable) -> List[Union[set, Any]]:
-#     """Estructura del que pot ser la funció de creació de sets
-#     """
-#     # groups = list()
-#     compares = dict()
-#     for i, sample_first in enumerate(csv_table):
-#         for sample_two in csv_table[1 + i:]:
-#             id1, id2 = sample_first['Accession'], sample_two['Accession']
-#             result = fasta.compare_samples(id1, id2)
-#             if result > 0.7:
-#                 compares.setdefault(id1, set())
-#                 compares[id1].add(id2)
-#
-#     list_relations = generate_relations(compares)
-#
-#     return list_relations
-#
-#
-# def generate_relations(compares):
-#     list_relations = []
-#     for elements in compares.keys():
-#         _, tree = explore_relations(compares, elements)
-#         if tree not in list_relations:
-#             list_relations.append(tree)
-#     return list_relations
-#
-#
-# def explore_relations(table, root, path=None, _sets=None):
-#     if _sets is None:
-#         _sets = set()
-#     if path is None:
-#         path = []
-#
-#     path += [root]
-#     _sets.add(root)
-#     _sets.update(table.get(root, ""))
-#     for neighbor in table.get(root, ""):
-#         if neighbor not in path:
-#             path, _sets = explore_relations(table, neighbor, path, _sets)
-#
-#     return path, _sets
-
 
 if __name__ == '__main__':
     if len(argv) != 2:
@@ -59,5 +16,5 @@ if __name__ == '__main__':
 
     fasta = FastaMap(fasta_path)
     csv_table = CsvTable(csv_path).filter()
-    
-    print(csv_table)
+    groups = fasta.group_samples(csv_table)
+    print(groups)
--- src/utils/fasta_map.py
+++ src/utils/fasta_map.py
@@ -1,6 +1,7 @@
 from typing import Tuple, Dict, List, Union, Any
 from utils.csv_table import CsvTable
 import time
+from pprint import pprint
 
 
 class FastaMap:
@@ -25,7 +26,7 @@ class FastaMap:
             sequences = filter(None, fasta.read().split('>'))
             for seq in sequences:
                 rna_id, rna = self._get_rna(seq)
-                data[rna_id] = rna
+                data[rna_id] = rna if len(rna) < 1000 else rna[:1000]
         return data
 
     def compare_samples(self, id1: str, id2: str) -> float:
@@ -33,9 +34,48 @@ class FastaMap:
         Arguments: id1, id2 -> String
         return: float
         """
-        #rna1, rna2 = self[id1], self[id2]
-        # TODO: SEQUENCE ALIGNMENT
-        return 0
+        s1, s2 = self[id1], self[id2]
+        matrix = self._align_sequences(s1, s2)
+        result = self._optimal_alignment(matrix, s1, s2)
+        return result
+
+    @staticmethod
+    def _optimal_alignment(matrix, s1, s2, gap=-2):
+        i, j = len(s1), len(s2)
+        matches, length = 0, 0
+        while (i > 0 or j > 0):
+            if i > 0 and j > 0 and matrix[i][j] == matrix[i-1][j-1] + FastaMap.__is_match(s1[i - 1], s2[j - 1]):
+                if s1[i - 1] == s2[j - 1]:
+                    matches += 1
+                i -= 1
+                j -= 1
+            elif i > 0 and matrix[i][j] == matrix[i-1][j] + gap:
+                i -= 1
+            else:
+                j -= 1
+            length += 1
+        return matches / length
+
+    @staticmethod
+    def _align_sequences(s1, s2, gap=-2):
+        s1_len, s2_len = len(s1), len(s2)
+        matrix = [[0 for _ in range(s2_len + 1)] for _ in range(s1_len + 1)]
+        for i in range(s1_len + 1):
+            matrix[i][0] = gap * i
+        for j in range(s2_len + 1):
+            matrix[0][j] = gap * j
+        for i in range(1, s1_len + 1):
+            for j in range(1, s2_len + 1):
+                match = matrix[i-1][j-1] + \
+                    FastaMap.__is_match(s1[i - 1], s2[j - 1])
+                delete = matrix[i - 1][j] + gap
+                insert = matrix[i][j - 1] + gap
+                matrix[i][j] = max(match, delete, insert)
+        return matrix
+
+    @staticmethod
+    def __is_match(x, y, match=1, mismatch=-1):
+        return match if x == y or x == 'N' or y == 'N' else mismatch
 
     @staticmethod
     def _get_rna(genome_info_str: str) -> Tuple[str, str]:
@@ -48,45 +88,45 @@ class FastaMap:
         genome_id = header.split('|')[0].strip()
         return genome_id, genome
 
-    # def group_samples(self, csv_table: CsvTable) -> List[Union[set, Any]]:
-    #     """Estructura del que pot ser la funció de creació de sets
-    #     """
-    #     star_time = time.time()
-    #     compares = dict()
-    #     list_relations = ()
-    #     for i, sample_first in enumerate(csv_table):
-    #         for sample_two in csv_table[1 + i:]:
-    #             id1, id2 = sample_first['Accession'], sample_two['Accession']
-    #             result = self.compare_samples(id1, id2)
-    #             if result > 0.9:
-    #                 compares.setdefault(id1, set())
-    #                 compares[id1].add(id2)
-    #     if compares:
-    #         list_relations = self.generate_relations(compares)
-    #     end_time = time.time() - star_time
-    #     print(end_time)
-    #     return list_relations
+    def group_samples(self, csv_table: CsvTable) -> List[Union[set, Any]]:
+        """Estructura del que pot ser la funció de creació de sets
+        """
+        star_time = time.time()
+        compares = dict()
+        list_relations = ()
+        for i, sample_first in enumerate(csv_table):
+            for sample_two in csv_table[1 + i:]:
+                id1, id2 = sample_first['Accession'], sample_two['Accession']
+                result = self.compare_samples(id1, id2)
+                if result > 0.9:
+                    compares.setdefault(id1, set())
+                    compares[id1].add(id2)
+        if compares:
+            list_relations = self.generate_relations(compares)
+        end_time = time.time() - star_time
+        print(end_time)
+        return list_relations
 
-    # @staticmethod
-    # def generate_relations(compares):
-    #     list_relations = ()
-    #     for elements in compares.keys():
-    #         _, tree = FastaMap.explore_relations(compares, elements)
-    #         if tree not in list_relations:
-    #             list_relations = list_relations + (tree,)
-    #     return list_relations
+    @staticmethod
+    def generate_relations(compares):
+        list_relations = ()
+        for elements in compares.keys():
+            _, tree = FastaMap.explore_relations(compares, elements)
+            if tree not in list_relations:
+                list_relations = list_relations + (tree,)
+        return list_relations
 
-    # @staticmethod
-    # def explore_relations(table, root, path=None, _sets=None):
-    #     if _sets is None:
-    #         _sets = set()
-    #     if path is None:
-    #         path = []
-    #     path += [root]
-    #     _sets.add(root)
-    #     _sets.update(table.get(root, ""))
-    #     for neighbor in table.get(root, ""):
-    #         if neighbor not in path:
-    #             path, _sets = FastaMap.explore_relations(
-    #                 table, neighbor, path, _sets)
-    #     return path, _sets
+    @staticmethod
+    def explore_relations(table, root, path=None, _sets=None):
+        if _sets is None:
+            _sets = set()
+        if path is None:
+            path = []
+        path += [root]
+        _sets.add(root)
+        _sets.update(table.get(root, ""))
+        for neighbor in table.get(root, ""):
+            if neighbor not in path:
+                path, _sets = FastaMap.explore_relations(
+                    table, neighbor, path, _sets)
+        return path, _sets
