806941ec0b7dc583b24a0f40fe7c4faff527c524
manudiv16 <manudiv16@gmail.com>
2020-05-10 21:23:10 +0200

    compares implemented

--- src/utils/fasta_map.py
+++ src/utils/fasta_map.py
@@ -1,4 +1,5 @@
 import concurrent.futures
+import os
 import time
 from collections import namedtuple
 from multiprocessing.dummy import Pool
@@ -20,15 +21,54 @@ class FastaMap:
             raise KeyError('Id not found')
         return self.__data[rna_id]
 
-    def compare_samples(self, id1, id2):
+    def compare_samples(self, id1: str, id2: str) -> float:
         """Compares to rna codes
         Arguments: id1, id2 -> String
         return: float
         """
+        s1, s2 = self[id1], self[id2]
+        matrix = self._align_sequences(s1, s2)
+        result = self._optimal_alignment(matrix, s1, s2)
+        return result
 
-        compare_module = ctypes.CDLL('src/utils/compares.so')
-        rna1, rna2 = self[id1], self[id2]
-        return compare_module.compares(rna1, rna2)
+    @staticmethod
+    def _optimal_alignment(matrix, s1, s2, gap=-2):
+        i, j = len(s1), len(s2)
+        matches, length = 0, 0
+        while i > 0 or j > 0:
+            if i > 0 and j > 0 and matrix[i][j] == matrix[i - 1][j - 1] + FastaMap.__is_match(s1[i - 1], s2[j - 1]):
+                if s1[i - 1] == s2[j - 1]:
+                    matches += 1
+                i -= 1
+                j -= 1
+            elif i > 0 and matrix[i][j] == matrix[i - 1][j] + gap:
+                i -= 1
+            else:
+                j -= 1
+            length += 1
+        print("completed", matches)
+        return matches / length
+
+    @staticmethod
+    def _align_sequences(s1, s2, gap=-2):
+        s1_len, s2_len = len(s1), len(s2)
+        matrix = [[0 for _ in range(s2_len + 1)] for _ in range(s1_len + 1)]
+        for i in range(s1_len + 1):
+            matrix[i][0] = gap * i
+        for j in range(s2_len + 1):
+            matrix[0][j] = gap * j
+        for i in range(1, s1_len + 1):
+            for j in range(1, s2_len + 1):
+                match = matrix[i - 1][j - 1] + \
+                        FastaMap.__is_match(s1[i - 1], s2[j - 1])
+                delete = matrix[i - 1][j] + gap
+                insert = matrix[i][j - 1] + gap
+                matrix[i][j] = max(match, delete, insert)
+        return matrix
+
+    @staticmethod
+    def __is_match(x, y, match=1, mismatch=-1):
+        return match if x == y or x == 'N' or y == 'N' else mismatch
 
     def _read_fasta(self, file_path: str) -> Dict[str, str]:
         """Reads a fasta file and returns a dict where the keys are the accessions
@@ -39,7 +79,7 @@ class FastaMap:
             sequences = filter(None, fasta.read().split('>'))
             for seq in sequences:
                 rna_id, rna = self._get_rna(seq)
-                data[rna_id] = rna
+                data[rna_id] = rna if len(rna) < 1000 else rna[:1000]
         return data
 
     @staticmethod
@@ -61,12 +101,13 @@ class FastaMap:
                       for i, sample_first in enumerate(csv_table)
                       for sample_two in csv_table[1 + i:]]
         compares = dict()
-        results = [self.compare_multi(x) for x in to_compare]
+        p = Pool()
+        results = p.map(self.compare_multi, to_compare)
         for x in results:
             compares.setdefault(x.id1, set())
             compares[x.id1].add(x.id2)
         list_relations = FastaMap.generate_relations(compares)
-        print(time.time()-fr)
+        print(time.time() - fr)
         return list_relations
 
     def compare_multi(self, ids):
