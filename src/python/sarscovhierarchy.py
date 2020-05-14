cfccff566f53769e38cdc93d13e6f6c02f9e2921
JasterV <victorcoder2@gmail.com>
2020-05-14 17:34:17 +0200

    improved the choice of the number of threads the computer can use

--- src/python/sarscovhierarchy.py
+++ src/python/sarscovhierarchy.py
@@ -1,15 +1,39 @@
 from sys import argv, exit
+from os import environ, popen
 from os.path import join
+import sys
 from utils.csv_table import CsvTable
 from utils.fasta_map import FastaMap
 import psutil
 import math
 
+
 def calcule_comparisons(n):
     result = 0
-    for i in range(1, n + 1):
+    for i in range(1, n):
         result += i
     return result
+
+
+def get_mem_available():
+    mem = psutil.virtual_memory()
+    return mem.available / 1000000000
+
+
+def get_threads():
+    """ Returns the number of available threads on a posix/win based system """
+    if sys.platform == 'win32':
+        return int(environ['NUMBER_OF_PROCESSORS'])
+    else:
+        return int(popen('grep -c cores /proc/cpuinfo').read())
+
+
+def get_max_threads(mem_available):
+    threads_available = get_threads()
+    num_threads = math.floor(mem_available/2)
+    max_threads = num_threads if num_threads <= threads_available else threads_available
+    return max_threads
+
 
 if __name__ == '__main__':
     if len(argv) != 2:
@@ -27,27 +51,26 @@ if __name__ == '__main__':
         .filter(lambda item: item[0] in ids)
     print("Files processing finished!")
 
-    mem = psutil.virtual_memory()
-    gigas_available = mem.available / 1000000000
-    num_threads = math.floor(gigas_available/2)
-
-    print(f"gigas available {gigas_available}, num threads: {num_threads}")
-
+    mem_available = get_mem_available()
+    max_threads = get_max_threads(mem_available)
     num_samples = len(fasta_map)
     num_comparisons = calcule_comparisons(num_samples)
 
-    print(f"\nThere are {num_samples} samples to compare in order to build the hierarchy.")
-    print(f"So that means the program will need to perform {num_comparisons} comparisons!")
+    print(
+        f"\nThere are {num_samples} samples to compare in order to build the hierarchy.")
+    print(
+        f"So that means the program will need to perform {num_comparisons} comparisons!")
     print("The algorithm implemented to compare 2 samples allocates a lot of memory (Up to 2 GB of memory per comparison in the worst case)")
 
-    answer = input("\nDo you want to perform the comparisons using multi-threading? (Yes/No) ").strip().lower()
+    answer = input(
+        "\nDo you want to perform the comparisons using multi-threading? (Yes/No) ").strip().lower()
     while answer != "yes" and answer != "no":
-        answer = input("\nDo you want to perform the comparisons using multi-threading? (Yes/No) ").strip().lower()
+        answer = input(
+            "\nDo you want to perform the comparisons using multi-threading? (Yes/No) ").strip().lower()
 
     if answer == "yes":
-        print(f"\nYou have {gigas_available} GB available, so we set the maximum threads the execution will use to {num_threads}!")
-        fasta_map.build_hierarchy(f"{num_threads}")
+        print(
+            f"\nYou have {mem_available} GB available, so we set the maximum threads to {max_threads}!")
+        fasta_map.build_hierarchy(f"{max_threads}")
     else:
         fasta_map.build_hierarchy("single")
-
-
