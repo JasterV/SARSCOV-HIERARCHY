83e886192eded9316baa4eace87d540bf314fc34
JasterV <victorcoder2@gmail.com>
2020-05-19 18:57:29 +0200

    Code refactorized

--- src/python/sarscovhierarchy.py
+++ src/python/sarscovhierarchy.py
@@ -1,95 +1,74 @@
-import os
 import signal
+import os
 from os.path import join
-from sys import argv
+from sys import argv, exit
 
-import utils.process_info_utils as piu
+from utils.process_info_utils import ProcessInfo
 from utils.csv_table import CsvTable
 from utils.fasta_map import FastaMap
 
 
-def show_system_info(samples, comparisons, mem_per_sample, available):
-    print(
-        f"\nThere are {samples} samples to compare in order to build the hierarchy.")
-    print(
-        f"So that means the program will need to perform {comparisons} comparisons!")
-    print(
-        "The algorithm implemented to compare 2 samples allocates "
-        "a lot of memory (Up to {:.3f} GB of memory per "
-        "comparison in the worst case)".format(
-            mem_per_sample))
-    print(
-        "\nYour computer have {:.3f} GB's of memory available right now.".format(available))
+def ask(question):
+    answer = input(f"{question} (yes/no) ").strip().lower()
+    while answer not in ('yes', 'no'):
+        answer = input(f"\n{question} (yes/no) ").strip().lower()
+    return answer
 
 
-def sequence_decision(available_mam, threads_max, comparisons_num, fasta):
-    if available_mam > 4:
-        answer = input(
-            "\nYou have the required space available to use "
-            "multi-threading! Do you want to? (yes/no) ").strip().lower()
-        while answer not in ('yes', 'no'):
-            answer = input(
-                "\nYou have the required space available to "
-                "use multi-threading! Do you want to? (yes/no) ").strip().lower()
-        if answer == "yes":
+def check_threading(available_mem, max_threads):
+    if available_mem > 4:
+        answer = ask(
+            "You have the required space available to use multi-threading! Do you want to?")
+        if answer == 'yes':
             print(
                 f"\nThe maximum number of threads that can be "
-                f"used by the execution has been set to {threads_max}.")
-            return estimated_duration(threads_max, comparisons_num, fasta)
-        return estimated_duration(threads_max, comparisons_num, fasta)
-    print("\nDue to the space available in your memory,"
-          " you can't use multi-threading to perform the comparisons.")
-    return estimated_duration(1, comparisons_num, fasta)
+                f"used by the execution has been set to {max_threads}.")
+            return max_threads
+    return 1
 
 
-def estimated_duration(threads, comparisons, fasta_map):
+def build_hierarchy(fasta, piu):
+    threads = check_threading(piu.mem_available, piu.max_threads)
     print("\nEstimated duration: {0:.3f} minutes.\n".format(
-        piu.get_duration(threads, comparisons)))
-    if threads > 1:
-        return fasta_map.build_hierarchy(f"{threads}")
-    return fasta_map.build_hierarchy("single")
+        piu.estimated_duration(threads, piu.num_comparisons)))
+    threads_option = str(threads) if threads > 1 else "single"
+    fasta.build_hierarchy(threads_option)
 
+signal.signal(signal.SIGTSTP, signal.SIG_IGN)
 
-def run():
+if __name__ == '__main__':
+    if len(argv) != 2:
+        print("python sarscovhierarchy.py <data_path>")
+        exit()
+
     data_dir = argv[1]
     csv_path = join(data_dir, "sequences.csv")
     fasta_path = join(data_dir, "sequences.fasta")
 
     print("Reading and processing files...")
+
     csv_table = CsvTable(csv_path) \
         .group_countries_by_median_length()
     ids = csv_table.values('Accession')
     fasta_map = FastaMap(fasta_path) \
         .filter(lambda item: item[0] in ids)
+
     print("Files processing finished!")
 
     max_length = max(map(lambda x: int(x), csv_table.values("Length")))
     num_samples = len(fasta_map)
-    num_comparisons = piu.calcule_comparisons(num_samples)
-    mem_available = piu.get_mem_available()
-    max_mem_per_sample = piu.get_max_mem_per_sample(max_length)
-    max_threads = piu.get_max_threads(mem_available, max_mem_per_sample)
-    show_system_info(num_samples,
-                     num_comparisons,
-                     max_mem_per_sample,
-                     mem_available)
-    result = sequence_decision(mem_available,
-                               max_threads,
-                               num_comparisons, fasta_map)
-    print(result)
 
+    piu = ProcessInfo(num_samples, max_length)
+    piu.show_system_info()
 
-if __name__ == '__main__':
-    if len(argv) == 2:
-        pid = os.getpid()
+    try:
         pid_h = os.fork()
         if pid_h == 0:
-            run()
-        else:
-            try:
-                os.wait()
-            except KeyboardInterrupt:
-                os.kill(pid_h, signal.SIGKILL)
-                print("shutdown")
-    else:
-        print("python sarscovhierarchy.py <directory>")
+            build_hierarchy(fasta_map, piu)
+            exit(0)
+        os.wait()
+    except KeyboardInterrupt:
+        os.kill(pid_h, signal.SIGKILL)
+        print("Bye!")
+    except:
+        print("Unexpected Error")
--- src/python/utils/process_info_utils.py
+++ src/python/utils/process_info_utils.py
@@ -4,37 +4,66 @@ import sys
 from os import environ, popen
 
 
-def calcule_comparisons(n):
-    result = 0
-    for i in range(1, n):
-        result += i
-    return result
+class ProcessInfo():
+    def __init__(self, num_samples, max_length):
+        self.__samples = num_samples
+        self.__num_comparisons = self.__calcule_comparisons(num_samples)
+        self.__mem_available = self.__get_mem_available()
+        self.__max_mem_per_sample = self.__get_max_mem_per_sample(max_length)
+        self.__max_threads = self.__get_max_threads(
+            self.__mem_available, self.__max_mem_per_sample)
 
+    @property
+    def num_comparisons(self):
+        return self.__num_comparisons
 
-def get_mem_available():
-    mem = psutil.virtual_memory()
-    return mem.available / 1000000000
+    @property
+    def max_threads(self):
+        return self.__max_threads
 
+    @property
+    def mem_available(self):
+        return self.__mem_available
 
-def get_num_system_cores():
-    """ Returns the number of available threads on a posix/win based system """
-    if sys.platform == 'win32':
-        return int(environ['NUMBER_OF_PROCESSORS'])
-    else:
-        return int(popen('grep -c cores /proc/cpuinfo').read())
+    def estimated_duration(self, num_threads, num_comparisons):
+        comparison_duration = 3.5
+        return ((num_comparisons*comparison_duration)/num_threads)/60
 
+    def show_system_info(self):
+        print(
+            f"\nThere are {self.__samples} samples to compare in order to build the hierarchy.")
+        print(
+            f"So that means the program will need to perform {self.__num_comparisons} comparisons!")
+        print(
+            "The algorithm implemented to compare 2 samples allocates "
+            "a lot of memory (Up to {:.3f} GB of memory per "
+            "comparison in the worst case)".format(
+                self.__max_mem_per_sample))
+        print(
+            "\nYour computer have {:.3f} GB's of memory available right now.".format(self.__mem_available))
 
-def get_max_threads(mem_available, mem_per_sample):
-    threads_available = get_num_system_cores()
-    num_threads = math.floor(mem_available/mem_per_sample)
-    max_threads = num_threads if num_threads <= threads_available else threads_available
-    return max_threads
+    def __calcule_comparisons(self, n):
+        result = 0
+        for i in range(1, n):
+            result += i
+        return result
 
+    def __get_mem_available(self):
+        mem = psutil.virtual_memory()
+        return mem.available / 1000000000
 
-def get_duration(num_threads, num_comparisons):
-    comparison_duration = 3.5
-    return ((num_comparisons*comparison_duration)/num_threads)/60
+    def __get_num_system_cores(self):
+        """ Returns the number of available threads on a posix/win based system """
+        if sys.platform == 'win32':
+            return int(environ['NUMBER_OF_PROCESSORS'])
+        else:
+            return int(popen('grep -c cores /proc/cpuinfo').read())
 
+    def __get_max_threads(self, mem_available, mem_per_sample):
+        threads_available = self.__get_num_system_cores()
+        num_threads = math.floor(mem_available/mem_per_sample)
+        max_threads = num_threads if num_threads <= threads_available else threads_available
+        return max_threads
 
-def get_max_mem_per_sample(max_sample_length):
-    return ((max_sample_length**2)*2)/1000000000
+    def __get_max_mem_per_sample(self, max_sample_length):
+        return ((max_sample_length**2)*2)/1000000000
